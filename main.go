68b430ef10c19686236ada6528d1ec1f6f39f2b9
YutakaHorikawa <gozutakemaru@gmail.com>
2016-08-18 16:00:51 +0900

    file upload functin

--- main.go
+++ main.go
@@ -7,10 +7,41 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 )
+
+// /uploadを処理するロジック
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //リクエストを受け取るメソッド
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload/upload.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       //オプションを解析します。デフォルトでは解析しません。
@@ -53,6 +84,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
 	http.HandleFunc("/", sayhelloName)       //アクセスのルーティングを設定します。
 	err := http.ListenAndServe(":9090", nil) //監視するポートを設定します。
 	if err != nil {
